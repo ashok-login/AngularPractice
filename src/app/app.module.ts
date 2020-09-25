0344f65f89eca3a9079bdcd55eed7afa2fd6bf1e
K Ashok Kumar <kak.mca@hotmail.com>
2020-09-25 20:59:08 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RichTextEditorComponent } from './rich-text-editor/rich-text-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RichTextEditorComponent
   ],
   imports: [
     BrowserModule,
